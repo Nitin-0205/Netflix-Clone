8af13eebcdd326ae6bef792640e40276c0c83034
Nitin Gupta <nitin1906@gmail.com>
2021-08-10 19:19:14 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,28 +8,28 @@ function Footer(){
             <p>Questions? Call 000-800-040-1843</p>
             <div className = "foot_list">
                 <ul>
-                    <li><a>FAQ</a></li>
-                    <li><a>Investor Relations</a></li>
-                    <li><a>Privacy</a></li>
-                    <li><a>Speed Test</a></li>
+                    <li><a href ="#">FAQ</a></li>
+                    <li><a href ="#">Privacy</a></li>
+                    <li><a href ="#">Investor Relations</a></li>
+                    <li><a href ="#">Speed Test</a></li>
                 </ul>
                 <ul>
-                    <li><a>Help Centre</a></li>
-                    <li><a>Jobs</a></li>
-                    <li><a>Cookie Preferences</a></li>
-                    <li><a>Legal Notices</a></li>
+                    <li><a href ="#">Help Centre</a></li>
+                    <li><a href ="#">Jobs</a></li>
+                    <li><a href ="#">Cookie Preferences</a></li>
+                    <li><a href ="#">Legal Notices</a></li>
                 </ul>
                 <ul>
-                    <li><a>Account</a></li>
-                    <li><a>Ways to Watch</a></li>
-                    <li><a>Corporate Information</a></li>
-                    <li><a>Only on Netflix</a></li>
+                    <li><a href ="#">Account</a></li>
+                    <li><a href ="#">Ways to Watch</a></li>
+                    <li><a href ="#">Corporate Information</a></li>
+                    <li><a href ="#">Only on Netflix</a></li>
                 </ul>
                 <ul>
-                    <li><a>Media Centre</a></li>
-                    <li><a>Terms of Use</a></li>
-                    <li><a>Privacy</a></li>
-                    <li><a>Contact Us</a></li>
+                    <li><a href ="#">Media Centre</a></li>
+                    <li><a href ="#">Terms of Use</a></li>
+                    <li><a href ="#">Privacy</a></li>
+                    <li><a href ="#">Contact Us</a></li>
                 </ul>
             </div>
             <div className="foot_dropdown_container">
